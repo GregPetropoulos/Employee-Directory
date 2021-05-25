142dc63e522aba61bce367fe58c9a9ff912d8a71
GP <gregpetropoulos@yahoo.com>
2021-05-25 07:49:05 -0400

    I have React rendering a table heading to a webpage

--- src/components/EmployeeContainer/index.js
+++ src/components/EmployeeContainer/index.js
@@ -6,39 +6,50 @@ import API from "../../utils/API";
 
 class EmployeeContainer extends Component {
   state = {
-  search: ""
+  search: "",
   employees:[],
   filteredEmployees: []
   };
 
   // When this component mounts, search for the movie "The Matrix"
-  componentDidMount() {
-    this.searchMovies("The Matrix");
-  }
+  // componentDidMount() {
+  //   this.searchMovies("The Matrix");
+  // }
 
-  searchMovies = query => {
-    API.search(query)
-      .then(res => this.setState({ result: res.data }))
-      .catch(err => console.log(err));
-  };
+  // searchMovies = query => {
+  //   API.search(query)
+  //     .then(res => this.setState({ result: res.data }))
+  //     .catch(err => console.log(err));
+  // };
 
-  handleInputChange = event => {
-    const value = event.target.value;
-    const name = event.target.name;
-    this.setState({
-      [name]: value
-    });
-  };
+  // handleInputChange = event => {
+  //   const value = event.target.value;
+  //   const name = event.target.name;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
-  // When the form is submitted, search the OMDB API for the value of `this.state.search`
-  handleFormSubmit = event => {
-    event.preventDefault();
-    this.searchMovies(this.state.search);
-  };
+  // // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   this.searchMovies(this.state.search);
+  // };
 
   render() {
      return (
-    //   <Container>
+       <>
+       <div className = 'container mt'>
+         <EmployeeTable
+         state={this.state}
+         sortBy={this.sortBy}
+        //  filteredEmployees={this.filteredEmployees}
+        />
+         </div>
+       </>
+    //   
+    
+    //<Container>
     //     <Row>
     //       <Col size="md-8">
     //         <Card
--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -2,7 +2,7 @@ import React from "react";
 // import "./style.css"
 
 // Set table with columns and rows, pass props, return the table with sorts
-const Employeetable = (props) => {
+const EmployeeTable = (props) => {
   return (
     <table className="table table-dark table-striped table-hover text-center table-sortable">
       {/* header of table */}
@@ -18,7 +18,7 @@ const Employeetable = (props) => {
 
           <th scope="col"><span onClick={() => props.sortBy("phone")}>Phone</span></th>
 
-          <th scope = "col"><span onClick ={() => props.sortBy.("email")}>Email</span></th>
+          <th scope = "col"><span onClick ={() => props.sortBy("email")}>Email</span></th>
 
           <th scope ="col"><span onClick ={() => props.sortBy("dob","date")}>DOB</span></th>
         </tr>
