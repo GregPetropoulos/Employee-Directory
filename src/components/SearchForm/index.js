79d04b7bd754d1c050ba6bc3421c207914ac22bb
GP <gregpetropoulos@yahoo.com>
2021-05-28 06:12:10 -0400

    Deleted Filter component since I created the functions in EmployeeContainer

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-// import "./style.css";
 
 const SearchForm = (props) => {
 
     return(
         <nav className="navbar navbar-danger bg-danger justify-content-center">
-            <form className = "form-inline m-6" onSubmit={props.handleFormSubmit}>
+            <form className = "form-inline m-2" onSubmit={props.handleFormSubmit}>
                 <input
                 className ="form-control"
                 value ={props.value}
